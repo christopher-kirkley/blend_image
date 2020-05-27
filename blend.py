78410b3ae0b9b8be278db991f3ebb99434a4964c
Christopher Kirkley <ckirkley@gmail.com>
2020-05-27 05:39:54 -0700

    Working initial prototype.

--- blend.py
+++ blend.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 
-def resize_image(image):
+def resize(image):
     img = cv.imread(image, cv.IMREAD_UNCHANGED)
 
     print('OG: ', img.shape)
@@ -17,15 +17,14 @@ image_list = ['source/1.jpg', 'source/2.jpg', 'source/3.jpg']
 
 number_of_images = len(image_list)
 percent = 1/number_of_images
-func_input = []
 for index, image in enumerate(image_list):
-    image = resize_image(image)
-    func_input.append(image)
-    func_input.append(percent)
+    image = resize(image)
+    image_list[index] = image * percent
+combined_image = sum(image_list).astype("uint8")
+cv.imshow('combined', combined_image)
+cv.waitKey(0)
 
-print(func_input)
 
 # dst = cv.addWeighted(img1, 0.5, img4, 0.5, 0)
 # cv.imshow('dst', dst)
-# cv.waitKey(0)
 # cv.destroyAllWindows()
